33207035d38ce0dc372f56558ff2795917671966
Vidy2 <111710984+Vidy2@users.noreply.github.com>
2026-05-10 19:21:35 -0700

    God I hate this

--- connect-go.py
+++ connect-go.py
@@ -5,7 +5,7 @@ import os
 
 today = datetime.now().strftime('%Y-%m-%d')
 
-url = f"https://nytimes.com{today}.json"
+url = f"https://www.nytimes.com/svc/connections/v2/{today}.json"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
